9d76be6da22bdbf4a1379ee7124e7b9b5c579e45
Sean Smith <sean.smith.2009@gmail.com>
2016-11-05 14:36:55 -0700

    updated host

--- src/client/constants/host.js
+++ src/client/constants/host.js
@@ -1,3 +1,3 @@
 
 export const DEV_HOST = 'http://127.0.0.1:5000'
-export const PROD_HOST = 'https://hidden-woodland-32853.herokuapp.com'
+export const PROD_HOST = 'http://hidden-woodland-32853.herokuapp.com'
